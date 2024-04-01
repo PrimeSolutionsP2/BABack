7394440dda0dfd3c4be15f65e814cd5d7950b14a
GianSz <giansz2003@gmail.com>
2024-03-31 22:38:13 -0500

    Fixed error when searching collection points by id

--- collectionPoint/infrastructure/entry-points/api-rest/src/main/java/com/collectionpoint/api/ApiRest.java
+++ collectionPoint/infrastructure/entry-points/api-rest/src/main/java/com/collectionpoint/api/ApiRest.java
@@ -7,10 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,7 +33,7 @@ public class ApiRest {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CollectionPoint> getById(@PathParam("id") int id) {
+    public ResponseEntity<CollectionPoint> getById(@PathVariable(name = "id") Integer id) {
         return new ResponseEntity<>(useCase.getById(id), HttpStatus.OK);
     }
 }
